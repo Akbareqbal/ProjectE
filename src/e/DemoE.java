18d37f8c138468b71a909f0325962d472ed9116c
akbar <akbar@Akbar.lan>
2024-01-07 13:08:16 -0600

    intial commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,15 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Friday");
+		
+		System.out.println("Team3");
+		System.out.println("Badge 11");
+		
+		
+	}
+
+}
